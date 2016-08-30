d6f69cb6740330e42312af28753ad6ee7740302c
Tamas Boros <tamas_boros@epam.com>
2016-08-30 10:35:08 +0200

    Locale config

---
+++ src/main/java/hu/borostomi/LocaleConfiguration.java
@@ -0,0 +1,34 @@
+package hu.borostomi;
+
+import java.util.Locale;
+
+import org.springframework.boot.autoconfigure.web.WebMvcProperties.LocaleResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+public class LocaleConfiguration extends WebMvcConfigurerAdapter{
+	 
+    @Bean(name="LocalResolver")
+    public SessionLocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.US);
+        return slr;
+    }
+ 
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+ 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+}
